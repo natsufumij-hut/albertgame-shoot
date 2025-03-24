df22cecb9b062d59d80ca123ef422bec03bfe74f
natsufumij <natsufumij@yeah.net>
2025-03-24 22:19:05 +0800

    添加分数、子弹

--- entities/player.gd
+++ entities/player.gd
@@ -8,18 +8,47 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 signal shoot_bullet(source_id: String, pos: Vector2, dir: Vector2)
+signal died_one(player_id: String, score: int)
+
+signal updat_hp(hp: int,hp_max: int)
+signal update_coin(coin: int)
+signal update_bullet(bullet:int, bullet_max: int)
+
 
 @export var player_id = "player1"
-@export var hp_max:int = 10
+@export var hp_max:int = 10:
+	set(value):
+		hp_max = value
+		updat_hp.emit(hp,value)
 @export var hp:int = 10:
 	set(value):
 		hp = value
+		updat_hp.emit(value,hp_max)
 		if value<=0:
 			died()
 		else:
 			update_flag()
+@export var score:int = 0:
+	set(value):
+		score = value
+		update_coin.emit(score)
 
-func _physics_process(delta: float) -> void:
+@export var player_drop_score:int = 3
+@export var bullet_num:int = 10:
+	set(value):
+		bullet_num = value
+		update_bullet.emit(bullet_num,bullet_max)
+@export var bullet_max:int = 10:
+	set(value):
+		bullet_max = value
+		update_bullet.emit(bullet_num,bullet_max)
+
+func reset_hp():
+	hp = hp_max
+	score = 0
+	bullet_num = bullet_max
+	
+func _physics_process(_delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -43,12 +72,21 @@ func _physics_process(delta: float) -> void:
 
 func check_inputs():
 	if Input.is_action_just_pressed("shoot"):
+		shoot_one()
+
+func shoot_one():
+	if bullet_num>0:
 		var angle = self.rotation+PI/2
 		var dir = Vector2(cos(angle),sin(angle))
 		var pos = marker_2d.global_position
 		shoot_bullet.emit(player_id,pos,dir)
+		bullet_num-=1
+		update_bullet.emit(bullet_num,bullet_max)
+		animation_player.play("shoot")
+
 
 func died():
+	died_one.emit(player_id,player_drop_score)
 	animation_player.play("explosion")
 
 func got_attacked():
@@ -57,6 +95,10 @@ func got_attacked():
 
 func update_flag():
 	var per = hp * 1.0 / hp_max
-	var now_frame = 1 - (sprite_2d.frame * 1.0 / sprite_2d.vframes)
-	if per< now_frame and (sprite_2d.frame<sprite_2d.vframes-1):
-		sprite_2d.frame += 1
+	if sprite_2d:
+		var now_frame = 1 - (sprite_2d.frame * 1.0 / sprite_2d.vframes)
+		if per< now_frame and (sprite_2d.frame<sprite_2d.vframes-1):
+			sprite_2d.frame += 1
+
+func got_coin(coin: int):
+	score += coin
--- scenes/solo_game_scene.gd
+++ scenes/solo_game_scene.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 const bullet = preload("res://entities/bullet.tscn")
 @onready var bullets: Node2D = $Bullets
+@onready var life_hp: Label = $HUD/Status/Life/Label
+@onready var score_label: Label = $HUD/Status/Score/Label
+@onready var bullet_label: Label = $HUD/Status/Shoot/Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$Player.reset_hp()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -19,3 +21,18 @@ func _on_player_shoot_bullet(source_id: String, pos: Vector2, dir: Vector2) -> v
 	bullet_ins.position = pos
 	bullets.add_child(bullet_ins)
 	
+
+
+func _on_player_updat_hp(hp: int, hp_max: int) -> void:
+	life_hp.text = String.num_int64(hp) +" / "+String.num_int64(hp_max)
+
+
+func _on_enemy_died_one(player_id: String, coin: int) -> void:
+	$Player.got_coin(coin)
+
+func _on_player_update_coin(coin: int) -> void:
+	score_label.text = String.num_int64(coin)
+
+
+func _on_player_update_bullet(bullet: int, bullet_max: int) -> void:
+	bullet_label.text = "%d / %d" % [bullet,bullet_max]
